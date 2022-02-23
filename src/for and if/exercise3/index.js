fccde2b6744ff66abad0342b01c70ffbdba2fe98
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 13:42:08 +0100

    ejercicios resueltos

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -1,7 +1,12 @@
 const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
+let contador=0;
 
-// Put your code here
+for (let item of numbers){
+    sum=sum+item;
+    contador++;
+}
+average=sum/contador;
 
-console.log(average)
+console.log(average);
--- src/for and if/exercise5/index.js
+++ src/for and if/exercise5/index.js
@@ -1,6 +1,10 @@
 const numbers = [0,1,5,2,7,5,0,0,9,5,5,6];
 let counter = 0;
 
-// Put your code here
+for(item of numbers){
+    if(item==5){
+        counter++;
+    }
+}
 
 console.log(counter)
