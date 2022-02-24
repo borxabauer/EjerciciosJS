f4ac137b49743efd9c89bb5add078294ad45a36e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 13:37:29 +0100

    img

--- src/functions/exercise7/example.js
+++ src/functions/exercise7/example.js
@@ -3,26 +3,26 @@ const numbers = [11,9,13,12,0,2,99,2,34,5,4,9,22,16,76,35,75,2,35,8,74]
 function minorInRange (start, end, array) {
     // Empiezo suponiendo que el primer elemento
     // del rango es el menor.
-    let minorIdx = start;
+    let minori = start;
     // Comparo el primer elemento del rango con
     // el siguiente y posteriores: start+1
-    for (let idx = start+1; idx < end; idx++) {
-        if ( array[minorIdx] > array[idx] ) {
-            minorIdx = idx;
+    for (let i = start+1; i < end; i++) {
+        if ( array[minori] > array[i] ) {
+            minori = i;
         }
     }
-    return minorIdx
+    return minori
 }
 
-function swap (idx1, idx2, array) {
-    const temp = array[idx1];
-    array[idx1] = array[idx2];
-    array[idx2] = temp;
+function swap (i1, i2, array) {
+    const temp = array[i1];
+    array[i1] = array[i2];
+    array[i2] = temp;
 }
 
 function sort (array, start = 0) {
-    let minorIdx = minorInRange(start, array.length, array);
-    if ( minorIdx > start ) swap(start, minorIdx, array);
+    let minori = minorInRange(start, array.length, array);
+    if ( minori > start ) swap(start, minori, array);
     if ( start < array.length ) sort(array, ++start)
 }
 
