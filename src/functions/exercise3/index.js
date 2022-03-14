429f76eead5b52f53b185bc0010409df8e7f9dab
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 13:40:39 +0100

    ejericios done

--- src/functions/exercise3/index.js
+++ src/functions/exercise3/index.js
@@ -1,7 +1,8 @@
 
 // Put your code here
+const numero = 8;
+const raizCubica = Math.pow(numero, 1 / 3);
 
-console.log(power(25))
-console.log(power(8,1/3))
-console.log(power(2,3))
-console.log(power(5,2))
+
+console.log(raizCubica);
+
