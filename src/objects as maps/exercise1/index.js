2c0cdc4786f034d1bc8c3a59b3eafddbb4a37021
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-21 09:01:21 +0100

    ejericios hechos

--- src/objects as maps/exercise1/index.js
+++ src/objects as maps/exercise1/index.js
@@ -1,15 +1,28 @@
 const phrase = "No hay bien que por mal no venga";
 const counters = {};
-function countLetter (letter, countersObject) {
-
-    // Put your code here
-
+function aGreatherThanB(a, b) {
+    return a > b;
 }
 
 
-for (letter of phrase) {
-    console.log(letter)
-    countLetter(letter, counters)
+function bGreatherThanA(a, b) {
+    return b > a;
+}
+
+function swap(idxA, idxB, array) {
+    const temp = array[idxA];
+    array[idxA] = array[idxB];
+    array[idxB] = temp;
+}
+
+function sort(comparator, array) {
+    for (let currentIdx = 0; currentIdx < array.length - 1; currentIdx++) {
+        for (let nextIdx = currentIdx + 1; nextIdx < array.length; nextIdx++) {
+            if (
+                comparator(array[currentIdx], array[nextIdx])
+            ) swap(currentIdx, nextIdx, array)
+        }
+    }
 }
 
 console.log(counters)
